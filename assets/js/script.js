5a63ccd094a3cd8a0b3ebbbe521660da9de37ea0
Jesse <jessecdenier@gmail.com>
2024-01-30 16:31:48 -0600

    Added other legs, and placeholder JS.

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+// Placeholder JS for the function to calculate percent of leg 1.
+$("#leg1ResultBtn").on("click", function () {
+  $("#leg1Result").text("100%");
+});
+
+// Placeholder JS for the function to calculate percent of leg 2.
+$("#leg2ResultBtn").on("click", function () {
+  $("#leg2Result").text("100%");
+});
+
+// Placeholder JS for the function to calculate percent of leg 3.
+$("#leg3ResultBtn").on("click", function () {
+  $("#leg3Result").text("100%");
+});
+
+// Placeholder JS for the function to calculate percent of leg 4.
+$("#leg4ResultBtn").on("click", function () {
+  $("#leg4Result").text("100%");
+});
+
+// Placeholder JS for the function to calculate percent of leg 5.
+$("#leg5ResultBtn").on("click", function () {
+  $("#leg5Result").text("100%");
+});
+
+// Placeholder JS for the function to calculate percent of leg 6.
+$("#leg6ResultBtn").on("click", function () {
+  $("#leg6Result").text("100%");
+});
+
+// Placeholder HS for the function to calculate total percent.
+$("#totalResultBtn").on("click", function () {
+  $("#totalResult").text("100%");
+});
