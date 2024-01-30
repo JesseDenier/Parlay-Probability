7412775655a37c35b918a011b9f5a5edce3be774
Jesse <jessecdenier@gmail.com>
2024-01-30 17:42:04 -0600

    Added javascript that toggles the form based on if leg is in use.

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,3 +32,25 @@ $("#leg6ResultBtn").on("click", function () {
 $("#totalResultBtn").on("click", function () {
   $("#totalResult").text("100%");
 });
+
+// Function to toggle visibility based on the "In Use" selection for a leg.
+// TODO: Learn what exactly legID is doing in this function.
+function toggleLegVisibility(legId) {
+  $(`#leg${legId}Use`).on("change", function () {
+    if ($(`#leg${legId}Use`).val() === "noUse") {
+      $(`#leg${legId}FormAfterUse`).hide();
+      $(`#leg${legId}ResultBtn`).hide();
+      $(`#leg${legId}Result`).hide();
+    } else {
+      $(`#leg${legId}FormAfterUse`).show();
+      $(`#leg${legId}ResultBtn`).show();
+      $(`#leg${legId}Result`).show();
+    }
+  });
+}
+
+// Loop through legs 1 through 6 and apply the toggleLegVisibility function to each leg.
+// TODO: Learn how the js knows which i value to use based on which leg you clicked.
+for (let i = 1; i <= 6; i++) {
+  toggleLegVisibility(i);
+}
