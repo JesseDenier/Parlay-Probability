a07f41d0f1159f336fb11565324daffedc0a463f
Jesse <jessecdenier@gmail.com>
2024-01-30 17:54:34 -0600

    Cleaned up javascript into for loops.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,27 @@
-// Placeholder JS for the function to calculate percent of leg 1.
-$("#leg1ResultBtn").on("click", function () {
-  $("#leg1Result").text("100%");
-});
+// Placeholder JS for the function to calculate percent of legs.
+// TODO: Learn how the js knows which i value to use based on which leg you clicked. (This happens later as well.)
+for (let i = 1; i <= 6; i++) {
+  $(`#leg${i}ResultBtn`).on("click", function () {
+    $(`#leg${i}Result`).text("100%");
+  });
+}
 
-// Placeholder JS for the function to calculate percent of leg 2.
-$("#leg2ResultBtn").on("click", function () {
-  $("#leg2Result").text("100%");
-});
-
-// Placeholder JS for the function to calculate percent of leg 3.
-$("#leg3ResultBtn").on("click", function () {
-  $("#leg3Result").text("100%");
-});
-
-// Placeholder JS for the function to calculate percent of leg 4.
-$("#leg4ResultBtn").on("click", function () {
-  $("#leg4Result").text("100%");
-});
-
-// Placeholder JS for the function to calculate percent of leg 5.
-$("#leg5ResultBtn").on("click", function () {
-  $("#leg5Result").text("100%");
-});
-
-// Placeholder JS for the function to calculate percent of leg 6.
-$("#leg6ResultBtn").on("click", function () {
-  $("#leg6Result").text("100%");
-});
-
-// Placeholder HS for the function to calculate total percent.
+// Placeholder JS for the function to calculate total percent.
 $("#totalResultBtn").on("click", function () {
   $("#totalResult").text("100%");
 });
 
-// Function to toggle visibility based on the "In Use" selection for a leg.
-// TODO: Learn what exactly legID is doing in this function.
-function toggleLegVisibility(legId) {
-  $(`#leg${legId}Use`).on("change", function () {
-    if ($(`#leg${legId}Use`).val() === "noUse") {
-      $(`#leg${legId}FormAfterUse`).hide();
-      $(`#leg${legId}ResultBtn`).hide();
-      $(`#leg${legId}Result`).hide();
+// Toggle visibility based on the "In Use" selection for each leg.
+for (let i = 1; i <= 6; i++) {
+  $(`#leg${i}Use`).on("change", function () {
+    if ($(`#leg${i}Use`).val() === "noUse") {
+      $(`#leg${i}FormAfterUse`).hide();
+      $(`#leg${i}ResultBtn`).hide();
+      $(`#leg${i}Result`).hide();
     } else {
-      $(`#leg${legId}FormAfterUse`).show();
-      $(`#leg${legId}ResultBtn`).show();
-      $(`#leg${legId}Result`).show();
+      $(`#leg${i}FormAfterUse`).show();
+      $(`#leg${i}ResultBtn`).show();
+      $(`#leg${i}Result`).show();
     }
   });
-}
-
-// Loop through legs 1 through 6 and apply the toggleLegVisibility function to each leg.
-// TODO: Learn how the js knows which i value to use based on which leg you clicked.
-for (let i = 1; i <= 6; i++) {
-  toggleLegVisibility(i);
 }
